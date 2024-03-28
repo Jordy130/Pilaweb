18af998e9adec33ea41a848a85536dbb07c5aca9
Jordy M.Velazquez Rojas <92339173+Jordy130@users.noreply.github.com>
2024-03-28 20:48:16 +0000

    ⭐Import: Cambios a var en index y usuarios

--- server/bin/www.js
+++ server/bin/www.js
@@ -1,90 +1,81 @@
-#!/usr/bin/env node
+#!/usr/bin/env node 
+/** 
+ * Module dependencies. 
+ */ 
+import app from '../app';
+import debugLib from 'debug';
+import http from 'http';
+const debug = debugLib('pilaweb:server');    
 
-/**
- * Module dependencies.
- */
+/** 
+* Normalize a port into a number, string, or false. 
+*/ 
+function normalizePort(val) { 
+const port = parseInt(val, 10); 
 
-var app = require('../app');
-var debug = require('debug')('pilaweb:server');
-var http = require('http');
+if (isNaN(port)) { 
+// named pipe 
+return val; 
+} 
+if (port >= 0) { 
+// port number 
+return port; 
+} 
+return false; 
+} 
 
-/**
- * Get port from environment and store in Express.
- */
+/** 
+* Get port from environment and store in Express. 
+*/ 
+var port = normalizePort(process.env.PORt || '3000'); 
+app.set('port', port); 
 
-var port = normalizePort(process.env.PORT || '3000');
-app.set('port', port);
+/** 
+* Event listener for HTTP server "error" event. 
+*/ 
+function onError(error) { 
+if (error.syscall !== 'listen') { 
+   throw error; 
+} 
 
-/**
- * Create HTTP server.
- */
+const bind = typeof port === 'string' 
+ ? 'Pipe ' + port
+ : 'Port ' + port; 
 
-var server = http.createServer(app);
+// handle specific listen errors with friendly messages 
+switch (error.code) { 
+case 'EACCES': 
+   console.error(bind + ' requires elevated privileges'); 
+   process.exit(1); 
+   break; 
+case 'EADDRINUSE': 
+   console.error(bind+ 'is already in use'); 
+   process.exit(1); 
+   break; 
+default: 
+   throw error; 
+} 
+} 
+ 
+/** 
+* Create HTTP server. 
+*/ 
+var server = http.createServer(app); 
 
-/**
- * Listen on provided port, on all network interfaces.
- */
+/** 
+* Event listener for HTTP server "listening" event. 
+*/ 
+function onListening() { 
+  const addr = server.address(); 
+  const bind = typeof addr === 'string'
+  ? 'pipe ' + addr
+  : 'port ' + addr.port;
+debug('Listening on ' + bind); 
+} 
 
-server.listen(port);
-server.on('error', onError);
+/** 
+ * Listen on provided port, on all network interfaces. 
+ */
+server.listen(port); 
+server.on('error', onError); 
 server.on('listening', onListening);
-
-/**
- * Normalize a port into a number, string, or false.
- */
-
-function normalizePort(val) {
-  var port = parseInt(val, 10);
-
-  if (isNaN(port)) {
-    // named pipe
-    return val;
-  }
-
-  if (port >= 0) {
-    // port number
-    return port;
-  }
-
-  return false;
-}
-
-/**
- * Event listener for HTTP server "error" event.
- */
-
-function onError(error) {
-  if (error.syscall !== 'listen') {
-    throw error;
-  }
-
-  var bind = typeof port === 'string'
-    ? 'Pipe ' + port
-    : 'Port ' + port;
-
-  // handle specific listen errors with friendly messages
-  switch (error.code) {
-    case 'EACCES':
-      console.error(bind + ' requires elevated privileges');
-      process.exit(1);
-      break;
-    case 'EADDRINUSE':
-      console.error(bind + ' is already in use');
-      process.exit(1);
-      break;
-    default:
-      throw error;
-  }
-}
-
-/**
- * Event listener for HTTP server "listening" event.
- */
-
-function onListening() {
-  var addr = server.address();
-  var bind = typeof addr === 'string'
-    ? 'pipe ' + addr
-    : 'port ' + addr.port;
-  debug('Listening on ' + bind);
-}
