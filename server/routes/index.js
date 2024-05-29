d2db0f229092478df367ae05cfe9afbd95ee147c
Jordy M.Velazquez Rojas <92339173+Jordy130@users.noreply.github.com>
2024-05-29 18:44:17 +0000

    ⭐ Feat: VsCode Auto Fix

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,10 @@
 import express from 'express';
+
 const router = express.Router();
 
-/* GET Home page */
-router.get('/', function(req, res, next ) {
+/* GET home page. */
+// eslint-disable-next-line no-unused-vars
+router.get('/', (req, res, next) => {
   res.render('index', { title: 'Express with Babel' });
 });
 
