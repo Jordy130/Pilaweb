d0f56616e421eb60e5cfbd88566e6e8a0415669b
Jordy M.Velazquez Rojas <92339173+Jordy130@users.noreply.github.com>
2024-05-30 07:00:16 +0000

    🌟 S30: Rutas de Acciones

--- server/domains/home/home.controller.js
+++ server/domains/home/home.controller.js
@@ -2,11 +2,17 @@
 // Get "/"
 // GET "/index"
 const home = (req, res) => {
-    const iconSet = ['⭐', '🤖', '🍉', '🐞', '🎄'];
-    const icon = iconSet[Math.floor(Math.random() * iconSet.length)];
-    res.render('index', { title: 'Express', icon });
-  };
-  
-  export default {
-    home,
-  };
+  const iconSet = ['⭐', '🤖', '🍉', '🐞', '🎄'];
+  const icon = iconSet[Math.floor(Math.random() * iconSet.length)];
+  res.render('index', { title: 'Express', icon });
+};
+
+// GET "/about"
+const about = (req, res) => {
+  res.send('⚠️ UNDER CONSTRUCTION: GET /about ⚠️');
+};
+
+export default {
+  home,
+  about,
+};
---
+++ server/domains/router.js
@@ -0,0 +1,45 @@
+import createError from 'http-errors';
+// Impornting winston logger
+import log from './config/winston';
+// Importando el enrutador Home
+import homeRouter from './domains/home/home.router';
+// Importando el enrutador User
+import userRouter from './domains/user/user.router';
+// Importando el enrutador Project
+import projectRouter from './domains/project/project.router';
+
+// Funcion que agrega las rutas
+const addRoutes = (app) => {
+  // Agregando las rutas de Home
+  app.use('/', homeRouter);
+
+  // Agregando las rutas de User
+  app.use('/user', userRouter);
+
+  // Agregando las rutas de Project
+  app.use('/project', projectRouter);
+
+  // 🚨 ERRORES 🚨
+  // catch 404 and forward to error handler
+  app.use((req, res, next) => {
+    log.info(`404 Pagina no encontrada 🤷‍♂️ ${req.method} ${req.originalUrl}`);
+    next(createError(404));
+  });
+
+  // error handler
+  // eslint-disable-next-line no-unused-vars
+  app.use((err, req, res, next) => {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+  });
+
+  return app;
+};
+
+// Exportando la funcion de enrutado
+export default { addRoutes }
