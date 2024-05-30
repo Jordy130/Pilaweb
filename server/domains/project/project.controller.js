bf7a8960859bc8f760f12fdb2169f3a0c144d97c
Jordy M.Velazquez Rojas <92339173+Jordy130@users.noreply.github.com>
2024-05-30 16:54:13 +0000

    🌟 Modelo Proyecto

---
+++ server/domains/project/project.controller.js
@@ -0,0 +1,17 @@
+// Actions methods
+
+// GET "/project"
+const showDashboard = (req, res) => {
+    res.send('⚠️ UNDER CONSTRUCTION: GET /project ⚠️');
+  };
+  
+  // GET "/project/add"
+  // GET "/project/add-form"
+  const addForm = (req, res) => {
+    res.render('project/addView');
+  };
+  
+  export default {
+    showDashboard,
+    addForm,
+  };
