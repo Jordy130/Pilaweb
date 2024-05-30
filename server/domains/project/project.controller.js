9a0a2efdacfc2384d22a916788c4d2255066464d
Jordy M.Velazquez Rojas <92339173+Jordy130@users.noreply.github.com>
2024-05-30 16:58:10 +0000

    🌟 Procesando formulario

--- server/domains/project/project.controller.js
+++ server/domains/project/project.controller.js
@@ -2,16 +2,22 @@
 
 // GET "/project"
 const showDashboard = (req, res) => {
-    res.send('⚠️ UNDER CONSTRUCTION: GET /project ⚠️');
-  };
-  
-  // GET "/project/add"
-  // GET "/project/add-form"
-  const addForm = (req, res) => {
-    res.render('project/addView');
-  };
-  
-  export default {
-    showDashboard,
-    addForm,
-  };
+  res.send('⚠️ UNDER CONSTRUCTION: GET /project ⚠️');
+};
+
+// GET "/project/add"
+// GET "/project/add-form"
+const addForm = (req, res) => {
+  res.render('project/addView');
+};
+
+// POST "/project/add"
+const addPost = (req, res) => {
+  res.status(200).json(req.body);
+};
+
+export default {
+  showDashboard,
+  addForm,
+  addPost,
+};
